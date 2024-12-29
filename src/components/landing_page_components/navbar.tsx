3122b4f26089114ebc359fe1a99b266bb7f5fbf4
Hritvik-Mohan <hritvik5005@gmail.com>
2024-12-29 23:57:13 +0530

    Update landing page

--- src/components/landing_page_components/navbar.tsx
+++ src/components/landing_page_components/navbar.tsx
@@ -1,18 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { FcMindMap } from "react-icons/fc";
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-between border-b-2 border-[rgba(243,243,243,1)] py-6 px-[10%]'>
+    <div>
+      <div className="flex items-center justify-between border-b-2 border-[rgba(243,243,243,1)] py-6 px-[10%]">
         <Link href="/dashboard">
-            <h1 className='flex items-center'><span className='font-bold'>Better</span><span>Anki</span><span className='ml-2'><FcMindMap size="1.5rem" /></span></h1>
+          <h1 className="flex items-center text-[1.5rem]">
+            <span className="font-bold">Better</span>
+            <span>Anki</span>
+            <span className="ml-2">
+              <FcMindMap size="2rem" />
+            </span>
+          </h1>
         </Link>
-        <div className='flex gap-2 text-[rgba(77,110,213,1)] font-medium'>
-            <Link href="/login">Login</Link>
-            <span>/</span>
-            <Link href="/signup">Signup</Link>
+        <div className="flex gap-2 text-[rgba(77,110,213,1)] font-semibold text-[1.2rem]">
+          <Link href="/login">Login</Link>
+          <span>/</span>
+          <Link href="/signup">Signup</Link>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,79 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <div>
-        {/* <Link href="/dashboard" className="flex justify-center">Go to dashboard</Link> */}
+      <div className="py-6 px-[10%]">
+        <h1 className="pb-[2rem] font-semibold text-gray-800 text-[2.5rem] sm:text-[3rem] md:text-[5rem] xl:text-[8rem] 2xl:text[10rem]">
+          Master Your Learning with Better{" "}
+          <span className="bg-[rgba(77,110,213,1)] text-white">Anki</span>
+        </h1>
+        <p className="font-normal text-gray-800 text-[1rem] sm:text-[1.5rem] md:text-[3rem] xl:text-[6rem] 2xl:text[8rem]">
+          An enhanced flashcard experience designed to{" "}
+          <span className="bg-yellow-400 text-white">supercharge</span> your{" "}
+          <span className="bg-orange-500 text-white">memory</span>{" "}
+          <span className="bg-green-500 text-white">retention</span> and make
+          studying fun.
+        </p>
+        <div className="">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg text-[1rem] xl:text[1.5rem] 2xl:text-[2rem] px-5 py-2.5 my-[2rem] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Get Started for Free
+          </button>
+        </div>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">Intelligent Spaced Repetition</div>
+          <p className="mb-[2rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+            Boost your long-term memory retention with adaptive scheduling based
+            on your learning pace.
+          </p>
+        </section>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">Intuitive and Modern Interface</div>
+          <p className="mb-[2rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          Enjoy a clean, user-friendly design that makes creating and managing cards effortless.
+          </p>
+        </section>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">Advanced Customization Options</div>
+          <p className="mb-[2rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          From rich-text formatting to media support, create flashcards that suit your learning style.
+          </p>
+        </section>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">Real-Time Collaboration</div>
+          <p className="mb-[2rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          Study with friends or share decks seamlessly.
+          </p>
+        </section>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">How It Works</div>
+          <p className="my-[1rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          <span className="font-extrabold">Step 1:</span> Create personalized flashcards in seconds.
+          </p>
+          <p className="mb-[1rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          <span className="font-extrabold">Step 2:</span> Study smarter with spaced repetition algorithms.
+          </p>
+          <p className="mb-[1rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          <span className="font-extrabold">Step 3:</span> Track your progress and keep improving.
+          </p>
+        </section>
+        <section className="mt-[4rem]">
+          <div className="my-[2rem]text-gray-800 font-semibold text-[2rem] sm:text-[2.5rem] md:text-[3rem] xl:text[3.5rem] 2xl:text-[4rem]">Why Better Anki?</div>
+          <p className="mb-[2rem] font-normal text-gray-800 text-[1rem] sm:text-[1.25rem] md:text-[1.5rem] xl:text[1.75rem] 2xl:text-[2rem]">
+          Better Anki was built for students, professionals, and lifelong learners who want to optimize their study process. Whether you're learning a new language, preparing for exams, or mastering a skill, Better Anki is here to make it easier.
+          </p>
+        </section>
+        <div className="">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg text-[1rem] xl:text[1.5rem] 2xl:text-[2rem] px-5 py-2.5 my-[2rem] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Get Started for Free
+          </button>
+        </div>
+        <section className="mb-[10rem]">
+        </section>
       </div>
     </div>
   );
