2e988dd910193fd9f4bf0140333d9c1f0b94b4d7
Hritvik-Mohan <hritvik5005@gmail.com>
2025-01-08 13:10:39 +0530

    UI Fix

--- src/components/dashboard_components/headmap/HeatMap.tsx
+++ src/components/dashboard_components/headmap/HeatMap.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import HeatMapCell from "./HeatMapCell";
 
 export default function HeatMap() {
-  const cells = Array.from({ length: 365 }, (_, i) => i)
+  const cells: number[] = Array.from({ length: 365 }, () => Math.round(Math.random()));
   return (
     <div className="overflow-x-auto">
         <div className="flex flex-wrap gap-1 min-w-[800px] max-w-[800px] my-4 overflow-x-auto">
           {cells.map((cell, index) => (
-            <HeatMapCell key={index}/>
+            <HeatMapCell key={index} isActive={cell === 1} />
           ))}
         </div>
     </div>
--- src/components/dashboard_components/headmap/HeatMapCell.tsx
+++ src/components/dashboard_components/headmap/HeatMapCell.tsx
@@ -6,12 +6,16 @@ import {
     TooltipTrigger,
   } from "@/components/ui/tooltip"
 
-export default function HeatMapCell() {
+  interface HeatMapCellProps {
+    isActive: boolean;
+  }
+
+export default function HeatMapCell({isActive}: HeatMapCellProps) {
   return (
     <TooltipProvider>
     <Tooltip>
       <TooltipTrigger asChild>
-      <div className='w-[10px] aspect-square bg-green-600 rounded-[2px]'></div>
+      <div className={`w-[10px] aspect-square rounded-[2px] ${isActive ? "bg-green-600" : "bg-gray-200"}`}></div>
       </TooltipTrigger>
       <TooltipContent>
         <p>3 new cards added and 12 cards reviewed on January 5th</p>
