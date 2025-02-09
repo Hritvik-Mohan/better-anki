b0dd6ce6786c4b13ef4f54d87ced5b2d341af956
Hritvik-Mohan <hritvik5005@gmail.com>
2025-02-09 19:42:35 +0530

    Add form handling in login and sign up page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,33 +1,97 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React, { useState } from "react";
 import { FcMindMap } from "react-icons/fc";
 import { IoChevronBack } from "react-icons/io5";
 
+interface FormState {
+  email: string;
+  password: string;
+}
+
 export default function Login() {
+  const [formState, setFormState] = useState<FormState>({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const { email, password } = formState;
+    if (!email || !password) {
+      setError("All fields are required");
+    } else {
+      setError('');
+      console.log('Logged in with:', { email, password });
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  console.log(formState);
 
   return (
-    <div className='flex bg-[rgba(228,228,228,1)] w-full h-screen items-center justify-center'>
-      <div className='bg-white max-w-[600px] rounded-lg px-8 pt-8 pb-12 mb-[8rem] mt-[4rem]'>
-      <Link href='/' className='flex items-center text-[rgba(77,110,213,1)]'>
-        <IoChevronBack /><p>home</p>
-      </Link>
-      <div className='flex justify-center mt-4'>
-        <FcMindMap size='7rem'/>
-      </div>
-        <h1 className='text-[2.5rem] mt-8'>Login</h1>
-        <p className='text-gray-500 my-4'>Anki requires an account to store your schedule, files, and progress and synchronize them across multiple devices.</p>
-        <div className='flex flex-col gap-4'>
-          <input className='bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg' type='email' placeholder='Email'/>
-          <input className='bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg' type='password' placeholder='Password'/>
+    <div className="flex bg-[rgba(228,228,228,1)] w-full h-screen items-center justify-center">
+      <div className="bg-white max-w-[600px] rounded-lg px-8 pt-8 pb-12 mb-[8rem] mt-[4rem]">
+        <Link href="/" className="flex items-center text-[rgba(77,110,213,1)]">
+          <IoChevronBack />
+          <p>home</p>
+        </Link>
+        <div className="flex justify-center mt-4">
+          <FcMindMap size="7rem" />
         </div>
-        <div className='flex justify-between mt-4 items-center'>
-          <p>Forgot password?</p> <button className='bg-[rgba(70,100,227,1)] text-white py-4 px-4 rounded-lg font-medium'>Login</button>
-        </div>
+        <h1 className="text-[2.5rem] mt-8">Login</h1>
+        <p className="text-gray-500 my-4">
+          Anki requires an account to store your schedule, files, and progress
+          and synchronize them across multiple devices.
+        </p>
+        <p className="mb-4 text-red-600 font-medium">
+          {error}
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4">
+            <input
+              className="bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              placeholder="Email your email"
+              required
+            />
+            <input
+              className="bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg"
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="flex justify-between mt-4 items-center">
+            <p>Forgot password?</p>{" "}
+            <button type="submit" className="bg-[rgba(70,100,227,1)] text-white py-4 px-4 rounded-lg font-medium">
+              Login
+            </button>
+          </div>
+        </form>
         <div className="flex justify-center my-4 h-[2px] rounded-lg bg-[rgba(243,243,243,1)]"></div>
-        <div className='flex justify-between mt-4 items-center'>
-          <Link href="/signup" className='text-gray-500'>Don&apos;t have an account yet?</Link> <button className='bg-[rgba(218,224,248,1)] text-[rgba(77,110,213,1)] py-4 px-4 rounded-lg font-medium'>Create Account</button>
+        <div className="flex justify-between mt-4 items-center">
+          <Link href="/signup" className="text-gray-500">
+            Don&apos;t have an account yet?
+          </Link>{" "}
+          <button className="bg-[rgba(218,224,248,1)] text-[rgba(77,110,213,1)] py-4 px-4 rounded-lg font-medium">
+            Create Account
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,35 +1,101 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React, { useState } from "react";
 import { FcMindMap } from "react-icons/fc";
 import { IoChevronBack } from "react-icons/io5";
 
-export default function Signup() {
+interface FormState {
+  username: string;
+  email: string;
+  password: string;
+}
 
+export default function Signup() {
+  const [formState, setFormState] = useState<FormState>({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const { email, password, username } = formState;
+    if (!email || !password || !username) {
+      setError("All fields are required");
+      console.log("All fields are required");
+    } else {
+      setError("");
+      console.log("Signed up with:", { email, password, username });
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
-    <div className='flex bg-[rgba(228,228,228,1)] w-full h-screen items-center justify-center'>
-      <div className='bg-white max-w-[600px] rounded-lg px-8 pt-8 pb-12 mb-[8rem]'>
-      <Link href='/' className='flex items-center text-[rgba(77,110,213,1)]'>
-        <IoChevronBack /><p>home</p>
-      </Link>
-      <div className='flex justify-center mt-4'>
-        <FcMindMap size='7rem'/>
-      </div>
-        <h1 className='text-[2.5rem] mt-8'>Signup</h1>
-        <p className='text-gray-500 my-4'>Anki requires an account to store your schedule, files, and progress and synchronize them across multiple devices.</p>
-        <div className='flex flex-col gap-4'>
-          <input className='bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg' type='username' placeholder='Name'/>
-          <input className='bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg' type='email' placeholder='Email'/>
-          <input className='bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg' type='password' placeholder='Password'/>
+    <div className="flex bg-[rgba(228,228,228,1)] w-full h-screen items-center justify-center">
+      <div className="bg-white max-w-[600px] rounded-lg px-8 pt-8 pb-12 mb-[8rem]">
+        <Link href="/" className="flex items-center text-[rgba(77,110,213,1)]">
+          <IoChevronBack />
+          <p>home</p>
+        </Link>
+        <div className="flex justify-center mt-4">
+          <FcMindMap size="7rem" />
         </div>
-        {/* <div className="flex justify-center my-4 h-[2px] rounded-lg bg-[rgba(243,243,243,1)]"></div> */}
-        <div className='flex justify-between mt-4 items-center'>
-          <Link href="/login" className='text-black'>Already have an account?</Link> <button className='bg-[rgba(70,100,227,1)] text-white py-4 px-4 rounded-lg font-medium'>Signup</button>
-        </div>
+        <h1 className="text-[2.5rem] mt-8">Signup</h1>
+        <p className="text-gray-500 my-4">
+          Anki requires an account to store your schedule, files, and progress
+          and synchronize them across multiple devices.
+        </p>
+        <p className="mb-4 text-red-600 font-medium">
+          {error}
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4">
+            <input
+              className="bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg"
+              type="username"
+              name="username"
+              value={formState.username}
+              onChange={handleChange}
+              placeholder="Name"
+            />
+            <input
+              className="bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <input
+              className="bg-[rgba(243,243,243,1)] p-4 outline-none rounded-lg"
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              placeholder="Password"
+            />
+          </div>
+          {/* <div className="flex justify-center my-4 h-[2px] rounded-lg bg-[rgba(243,243,243,1)]"></div> */}
+          <div className="flex justify-between mt-4 items-center">
+            <Link href="/login" className="text-black">
+              Already have an account?
+            </Link>{" "}
+            <button type="submit" className="bg-[rgba(70,100,227,1)] text-white py-4 px-4 rounded-lg font-medium">
+              Signup
+            </button>
+          </div>
+        </form>
         {/* <div className='flex justify-between mt-4 items-center'>
           <p className='text-gray-500'>Don't have an account yet?</p> <button className='bg-[rgba(218,224,248,1)] text-[rgba(77,110,213,1)] py-4 px-4 rounded-lg font-medium'>Login</button>
         </div> */}
       </div>
     </div>
-  )
+  );
 }
